c912e866fc7f0910cd95b2bc791df655ec835ec2
unknown <jaimerios@tutamail.com>
2020-12-20 13:56:13 -0800

    create BusinessList component and css file, correct import statements

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,5 +1,5 @@
 import React from 'react';
-@import './Business.css';
+import './Business.css';
 
 const business = {
   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
@@ -13,7 +13,7 @@ const business = {
   reviewCount: 90
 }
 
-class Business extends React.Component {
+export class Business extends React.Component {
   render() {
     return (
       <div className="Business">
