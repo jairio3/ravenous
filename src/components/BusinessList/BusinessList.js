085e82c677e6eb045adb95a37e73149c247fea9f
unknown <jaimerios@tutamail.com>
2020-12-20 14:12:04 -0800

    render Business components within BusinessList

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,3 +1,18 @@
 import React from 'react';
+import { Business } from '../Business/Business.js'
 import './BusinessList.css';
 
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div class="BusinessList">
+        <Business></Business>
+        <Business></Business>
+        <Business></Business>
+        <Business></Business>
+        <Business></Business>
+        <Business></Business>
+      </div>
+      );
+  }
+}
