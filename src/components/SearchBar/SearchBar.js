58a7f344587ccee93c075fbd3b39e4bd82ba6055
unknown <jaimerios@tutamail.com>
2020-12-20 16:57:56 -0800

    create sortByOptions obj to display within list items, create SearchBar component and renderSortOptions method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,3 +1,23 @@
 import React from 'react';
 import './SearchBar.css';
 
+const sortByOptions = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+};
+
+export class SearchBar extends React.Component {
+
+  renderSortByOptions() {
+    Object.keys(sortByOptions).map( (sortByOption) => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
+    });
+  }
+
+  render() {
+    return
+  }
+
+}
