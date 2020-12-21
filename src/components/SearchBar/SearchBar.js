ceccc9d4078b5b25ae65edc4ab8255865c560988
unknown <jaimerios@tutamail.com>
2020-12-20 17:03:12 -0800

    add jsx for rendering searchbar and searchbar options

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -17,7 +17,22 @@ export class SearchBar extends React.Component {
   }
 
   render() {
-    return
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions()}
+          </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
+      </div>
+      )
   }
 
 }
