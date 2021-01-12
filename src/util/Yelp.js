2df809191d9ea054ce285ad4091c1ecc4fc1fe88
unknown <jaimerios@tutamail.com>
2021-01-11 21:02:23 -0800

    changes to add for ravenous app (now completed):
    • simulate query to yelp api (remove hardcoded business info)
    • simulate a list of businesses to search for
    • install essential npm packages (dotenv, whatwg-fetch)

---
+++ src/util/Yelp.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const apiKey = process.env.REACT_APP_API_KEY;
+
+export const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+    {
+      headers: {
+        authorization: `Bearer ${apiKey}`
+      }
+    }).then( response => {
+      return response.json();
+    }).then( jsonResponse => {
+      if ('businesses' in jsonResponse) {
+        return jsonResponse.businesses.map( business => {
+          console.log(business)
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count
+          }
+        });
+      }
+    });
+  }
+}
